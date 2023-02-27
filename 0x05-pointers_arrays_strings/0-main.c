227405fabfcc4c40e603e3ade9389b6fd50622c1
Baitsumedi <baitsumedi.pv@gmail.com>
2023-02-27 06:43:36 -0800

    modifications

--- 0x05-pointers_arrays_strings/0-main.c
+++ 0x05-pointers_arrays_strings/0-main.c
@@ -1,8 +1,12 @@
+include "main.h"
+#include <stdio.h>
+
 /**
  * main - check the code 
  *
  * Return: Always 0.
  */
+
 int main(void)
 {
     int n;
---
+++ 0x05-pointers_arrays_strings/_putchar.c
@@ -0,0 +1,16 @@
+#include <unistd.h>
+
+/**
+* *_puchar -writes the character c to stdout
+* * @c: Character to be printed
+
+*   * Return: 1 on success
+
+*     * Return -1 when thre is an error, and error number is set
+
+*/
+
+int _puchar(char c)
+{
+    return (write(1, &c, 1));
+}
