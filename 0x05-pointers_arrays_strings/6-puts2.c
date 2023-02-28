287ca74037c44116494844fc1372d9dd9e4445a7
Baitsumedi <baitsumedi.pv@gmail.com>
2023-02-28 15:50:05 -0800

    6-puts2.c

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -1,6 +1,6 @@
 #include "main.h"
 /**
- * puts 2 - prints every other character, followed by a new line
+ * puts2 - prints every other character, followed by a new line
  * @str: the string from which we will print our characters
  */
 void puts2(char *str)
