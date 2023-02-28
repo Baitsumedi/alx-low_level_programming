7cab4a968d25e71813dab7409bee37f46f13b2ef
Baitsumedi <baitsumedi.pv@gmail.com>
2023-02-28 07:43:36 -0800

    Task 4, Print in reverse

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -8,13 +8,14 @@ void print_rev(char *s)
 int a, b, c;
 a = 0;
 *s = a;
-while (*s[a] != '\0')
+while (s[a] != '\0')
 {
 a++;
-c = i;
+c = a;
 for (b = c - 1; b >= 0; b--)
 {
 _putchar(s[b]);
 }
 _putchar('\n');
+
 }
